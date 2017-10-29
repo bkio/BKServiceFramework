5f03beb28266e18af291c51bbce1fff39047cd23
Burak Kara <burak@burak.io>
2017-10-29 02:00:34 -0700

    Linux fixes

--- source/WEngine/WSegvCatch.h
+++ source/WEngine/WSegvCatch.h
@@ -29,21 +29,11 @@
     #include <execinfo.h>
     #include <string>
 
-    extern "C"
-    {
-        struct KernelSigaction
-        {
-            void (*k_sa_sigaction)(int, siginfo_t *, void *);
-            unsigned long k_sa_flags;
-            void (*k_sa_restorer) ();
-            sigset_t k_sa_mask;
-        };
-    }
-
     #define MAKE_THROW_FRAME(_exception)
 
     /* The return code for realtime-signals.  */
     /* __NR_rt_sigreturn defined as 15 in unistd64.h */
+
     asm
     (
         ".text\n"
@@ -53,19 +43,16 @@
         "	movq $15, %rax\n"
         "	syscall\n"
     );
-    void restore_rt () asm ("__restore_rt")
-      __attribute__ ((visibility ("hidden")));
+    void restore_rt () asm ("__restore_rt") __attribute__ ((visibility ("hidden")));
 
     /* Unblock a signal.  Unless we do this, the signal may only be sent
        once.  */
     static void UnblockSignal(int signum __attribute__((__unused__)))
     {
-#ifdef _POSIX_VERSION
         sigset_t sigs{};
         sigemptyset(&sigs);
         sigaddset(&sigs, signum);
         sigprocmask(SIG_UNBLOCK, &sigs, nullptr);
-#endif
     }
 
     static void CatchSegv (int, siginfo_t *, void *_p __attribute__ ((__unused__)))
@@ -75,18 +62,22 @@
         throw std::runtime_error("Segmentation fault");
     }
 
+    struct KernelSigaction
+    {
+        void (*k_sa_sigaction)(int, siginfo_t *, void *);
+        unsigned long k_sa_flags;
+        void (*k_sa_restorer) ();
+        sigset_t k_sa_mask;
+    };
+
     void InitWSegv()
     {
-        do
-        {
-            struct KernelSigaction act{};
-            act.k_sa_sigaction = CatchSegv;
-            sigemptyset(&act.k_sa_mask);
-            act.k_sa_flags = SA_SIGINFO | 0x4000000;
-            act.k_sa_restorer = restore_rt;
-            syscall(SYS_rt_sigaction, SIGSEGV, &act, NULL, _NSIG / 8);
-        }
-        while (false);
+        KernelSigaction act{};
+        act.k_sa_sigaction = CatchSegv;
+        sigemptyset(&act.k_sa_mask);
+        act.k_sa_flags = SA_SIGINFO | 0x4000000;
+        act.k_sa_restorer = restore_rt;
+        syscall(SYS_rt_sigaction, SIGSEGV, &act, nullptr, _NSIG / 8);
     }
 #endif
 
